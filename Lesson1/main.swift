d2bb027a540f286de760bc13f6187b05a837e03f
LizOk <Lizok.96@bk.ru>
2021-03-05 13:55:13 +0300

    задание 1

--- Lesson1/main.swift
+++ Lesson1/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+
+func calculateQuadraticRoots(a: Double, b: Double, c: Double) {
+    let discriminant = sqrt(b * b - 4 * a * c);
+    let x1 = (-b + discriminant)/(2 * a);
+    let x2 = (-b - discriminant)/(2 * a);
+    print(x1); print(x2)
+    
+}
+
+calculateQuadraticRoots(a: 4.00, b: 8.00, c: 3.00);
+    
+    
 
