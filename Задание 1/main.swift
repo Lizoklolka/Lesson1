fa26c9bda073cb935b8e2d6c94c6ce01c2ddb8ae
LizOk <Lizok.96@bk.ru>
2021-03-07 12:29:33 +0300

    Lesson1

--- Задание 1/main.swift
+++ Задание 1/main.swift
@@ -7,16 +7,15 @@
 
 import Foundation
 
+// Задание 1
 
-//func calculateQuadraticRoots(a: Double, b: Double, c: Double) {
-//    let discriminant = sqrt(b * b - 4 * a * c);
-//    let x1 = (-b + discriminant)/(2 * a);
-//    let x2 = (-b - discriminant)/(2 * a);
-//    print("Первый корень уравнения равен ", x1); print("Второй корень уравнения равен ", x2)
-//
-//}
-//
-//calculateQuadraticRoots(a: 4.00, b: 8.00, c: 3.00);
+func calculateQuadraticRoots(a: Double, b: Double, c: Double) {
+    let discriminant = sqrt(b * b - 4 * a * c);
+    let x1 = (-b + discriminant)/(2 * a);
+    let x2 = (-b - discriminant)/(2 * a);
+    print("Первый корень уравнения равен ", x1); print("Второй корень уравнения равен ", x2)
+}
+calculateQuadraticRoots(a: 4.00, b: 8.00, c: 3.00);
 
 // Задание 2
 
@@ -24,10 +23,20 @@ func calculateTriangle(a: Double, b: Double) {
     let c: Double = sqrt(b * b + a * a);
     let P = a + b + c;
     let S = (a * b)/2;
-    
+
     print("Гипотенуза треугольника равна ", c);
     print("Периметр треугольника равен ", P);
     print("Площадь треугольника равна ",  S);
 }
     calculateTriangle(a: 4.00, b: 6.00)
+
+//Задание 3
+
+var sum: Double = 1500;
+var rate: Double = 5;
+rate = (rate / 100) + 1;
+for i in 1...5{
+    sum *= rate;
+}
+print("Размер вклада равен ", sum);
 
